6ea31b75601e294b06224f4e5daf148ddf770f01
TheCapn-MEC <erolda266@west-mec.org>
2021-09-20 21:00:54 -0700

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect} from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
+const url = 'https://course-api.com/react-tabs-project';
+
+const App = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default App
